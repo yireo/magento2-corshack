a675ae23465203e0d20bc717ee398a37c001bd05
jisse Reitsma <jisse@yireo.com>
2020-05-18 18:26:06 +0200

    Fix issues with Apollo

--- Plugin/ControllerPlugin.php
+++ Plugin/ControllerPlugin.php
@@ -12,13 +12,11 @@ declare(strict_types=1);
 
 namespace Yireo\CorsHack\Plugin;
 
-use Magento\Framework\App\Request\Http;
-use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Response\Http as HttpResponse;
-use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\Response\HttpInterface;
 use Magento\GraphQl\Controller\GraphQl as Source;
-use Magento\Framework\App\Response\Http as Response;
 use Yireo\CorsHack\Utils\ResponseGenerator;
+use Zend\Http\Request;
 
 /**
  * Class ControllerPlugin
@@ -33,26 +31,57 @@ class ControllerPlugin
     private $responseGenerator;
 
     /**
+     * @var Request
+     */
+    private $request;
+
+    /**
      * ActionPlugin constructor.
      *
      * @param ResponseGenerator $responseGenerator
+     * @param Request $request
      */
     public function __construct(
-        ResponseGenerator $responseGenerator
+        ResponseGenerator $responseGenerator,
+        Request $request
     ) {
         $this->responseGenerator = $responseGenerator;
+        $this->request = $request;
     }
 
     /**
      * @param Source $source
-     * @param $closure
-     * @param RequestInterface $request
-     *
-     * @return HttpResponse
+     * @param HttpResponse $response
+     * @return HttpInterface
      */
     public function afterDispatch(Source $source, HttpResponse $response)
     {
-        $response = $this->responseGenerator->modifyResponse($response);
-        return $response;
+        if ($this->spoofStatusHeaderIgnorantly($response)) {
+            $response->setStatusHeader(200);
+        }
+
+        return $this->responseGenerator->modifyResponse($response);
+    }
+
+    /**
+     * @param HttpResponse $response
+     * @return bool
+     */
+    private function spoofStatusHeaderIgnorantly(HttpResponse $response): bool
+    {
+        if ($this->request->getMethod() === 'OPTIONS') {
+            return true;
+        }
+
+        if ($this->request->getMethod() === 'GET') {
+            return true;
+        }
+
+        $data = json_decode($response->getBody(), true);
+        if (!empty($data) && !empty($data['data']) && empty($data['errors'])) {
+            return true;
+        }
+
+        return false;
     }
 }
